dcbb4d127a3fdba71bdc5b60af5cf4eb978270a7
ldliao <laura60629@gmail.com>
2017-06-15 14:18:34 -0700

    Add files via upload

--- SpectralCV/eeg_pipe.py
+++ SpectralCV/eeg_pipe.py
@@ -1,32 +1,94 @@
-# Setup 
+"""
+eeg_pipe.py
+pipeline for eeg data to do the coefficient of variation of power spectral density.
+
+"""
+
 import numpy as np
 import scipy as sp
 import scipy.io as io
 import scipy.signal as sig
-import math as math
-import random 
-from scipy import integrate
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import h5py
+import scv
 
-def getEEGdata(mydata_path, sub):
+def getEEGdata(mydata_path, channel_data, sub):
+    """
+    grabbing the EEG data 
+    
+    Parameters
+    ----------
+    mydata_path : String
+        set the path
+    channel_data : String
+        grab the particular channel
+    sub : integer
+        grab the particular subject
+    
+    Returns
+    -------
+    data : array-like 1d
+        return the data.
+        
+    """
     data_path = mydata_path
     matfile = io.loadmat(data_path + '%d.mat'%(sub), squeeze_me=True)
-    data = matfile['oz_rest_data']
+    data = matfile[channel_data]
     return data
 
 def sublist():
+    """
+    Returns
+    -------
+    subject list
+    
+    """
     return np.concatenate((np.arange(1000,1015), np.arange(2000,2014)),axis=0)
 
 def getFreq(data, fs, nperseg, noverlap):
+    """
+    getting the amount of frequency window for the x-axis.
+        
+    Parameters
+    ----------
+    data : array-like 1d
+        the data.
+    fs : integer
+        frequency.
+    nperseg : integer
+        desired window per segment.
+    noverlap : integer
+        desired overlapping of window.
+    
+    Returns
+    -------
+    length of amount of frequency window.
+        
+    """
     f_axis, f_time, spg = sig.spectrogram(data, fs=fs, nperseg=fs, noverlap=fs)
     return len(f_axis)
 
-def humanBread(sublist,fs,nperseg,noverlap):
-    bread = np.zeros((len(sublist), 501))
+def humanBread(sublist, fs, nperseg, noverlap):
+    """
+    getting the amount of frequency window for the x-axis.
+    
+    Parameters
+    ----------
+    sublist : list
+        subject list
+    fs : integer
+        frequency.
+    nperseg : integer
+        desired window per segment.
+    noverlap : integer
+        desired overlapping of window.
+    
+    Returns
+    -------
+    bread : array-like 3d
+           return the data- frequency x subject        
+    """
+    bread = np.zeros((len(sublist), getfreq(data, fs, nperseg, noverlap)))
     for i, sub in enumerate(sublist):
         data = getEEGdata(data_path,sub)
         f_axis, f_time, spg = sig.spectrogram(data, fs=fs, nperseg=nperseg, noverlap=noverlap)
-        bread[i][:] = ep.scv(spg)
+        bread[i][:] = scv.scv(spg)
     return bread
---
+++ SpectralCV/scv.py
@@ -0,0 +1,44 @@
+"""
+scv.py
+functions needed to do analysis for coefficient of variation of power spectral density.
+
+"""
+
+import numpy as np
+
+def scv(SP):
+    """
+    calculating the coefficient of variation
+    
+    Parameters
+    ----------
+    SP : array-like 1d
+        Spectrogram of data.
+    
+    Returns
+    -------
+    scv : array-like 1d
+        return the cofficient of variation by the spectrogram corresponds to the segment times.
+        
+    """
+    scv = ((np.std(SP,axis=1)/np.mean(SP,axis=1)))
+    return scv
+
+
+def psd(SP):
+    """
+    calculating the power spectral density.
+    
+    Parameters
+    ----------
+    SP : array-like 1d
+        Spectrogram of x.
+    
+    Returns
+    -------
+    psd : array-like 1d
+        return the mean of each segment by the spectrogram.
+        
+    """
+    psd = (np.mean(SP,axis=1))
+    return psd
