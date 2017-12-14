0fe61c233b4b0368f9f1908370e04f57170abc86
ldliao <laura60629@gmail.com>
2017-12-14 07:29:20 -0800

    updated newPipe notebook
    consist of how-to-use

--- codes/access_nt.py
+++ codes/access_nt.py
@@ -4,8 +4,28 @@ import scipy.io as io
 import scipy.signal as sig
 from neurodsp import spectral
 
-def get_ECoG(data_path, session, chan, indices):
-
+def get_ECoG(data_path, session, chan, indices = [0,0]):
+    ''' grabs certain chunks of data 
+    
+        Parameters
+        ----------
+        data_path : String
+            set the path
+        session : integer
+            grab the particular session
+        chan : list
+            grab the particular channels
+        indices : list
+            specify start and end for each session
+            initialized as whole if none is given
+            
+        Returns
+        -------
+        data : 2d array
+            return the data
+        timefile : file
+            return information about the session
+    '''
     data_path = data_path % (session)
     if "Session" not in data_path:
         raise DataError("check data!")
@@ -16,8 +36,11 @@ def get_ECoG(data_path, session, chan, indices):
     
     for c in chan:
         try:
-            matfile = io.loadmat(data_path + 'ECoG_ch%d.mat'%(c), squeeze_me=True)
+            matfile = io.loadmat(data_path + 'ECoG_ch%d.mat'%(c),
+                                 squeeze_me=True)
             data = matfile['ECoGData_ch%d'%(c)]
+            if indices == [0,0]:
+                indices = [0,len(data)]
             data_block.append(data[indices[0]:indices[1]])
         except Exception as e:
             print('Handling run-time error:', e)
@@ -25,13 +48,31 @@ def get_ECoG(data_path, session, chan, indices):
             data[:] = np.nan
             data_block.append(data)
     
+    data_block = np.vstack(data_block)
 
     return data_block, timefile
 
 def get_cond(timefile, start_ind, end_ind):
-	start = timefile['ConditionTime'][start_ind]
-	end = timefile['ConditionTime'][end_ind]
-	indices = [start, end]
-	return indices
+    ''' grabs indices
+       
+        Parameters
+        ----------
+        timefile : file
+            get information about the session
+        start_ind : int
+            specific condition start
+        end_ind : int
+            specific condition end
+           
+        Returns
+        -------
+        indices : list
+            return the start and end of the data of specific condition
+    '''
+    start = timefile['ConditionTime'][start_ind]
+    end = timefile['ConditionTime'][end_ind]
+    indices = [start, end]
+    
+    return indices
 
 
