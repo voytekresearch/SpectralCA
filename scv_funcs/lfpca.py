eee50960c447a77a7bb11a295311b60ce47d58cb
rdgao <r.dg.gao@gmail.com>
2018-05-24 13:07:34 -0700

    added plot exclusion frequency to plot_spectral

--- scv_funcs/lfpca.py
+++ scv_funcs/lfpca.py
@@ -220,22 +220,29 @@ class LFPCA:
             plt.plot(x, rv.pdf(x), 'k-', lw=2, label='Fit PDF')
         plt.legend()
         plt.xlabel('Spectral Power')
-        plt.ylabel('Probability')
-        plt.title('Frequency=%.1fHz, p=%.4f' %(self.f_axis[freq_ind],self.ks_pvals[chan,freq_ind]))
+        #plt.ylabel('Probability')
+        plt.title('%.1fHz, p=%.4f' %(self.f_axis[freq_ind],self.ks_pvals[chan,freq_ind]))
 
-    def plot_spectral(self, plot_mean=True, plot_chan=None, plot_color='k'):
+
+    def plot_spectral(self, plot_mean=True, plot_chan=None, plot_color='k', exc_freqs=None):
         if plot_chan is None:
             plot_chan = np.arange(0,self.numchan)
+
+        if exc_freqs is not None:
+            p_inds = _freq_to_ind(self.f_axis, exc_freqs)
+        else:
+            p_inds = np.arange(len(self.f_axis))
+
         titles = ['PSD', 'SCV', 'KS P-Val', 'KS Stats']
         plot_keys = ['psd', 'scv', 'ks_pvals', 'ks_stats']
         for i in range(4):
             plt.subplot(1,4,i+1)
             if plot_mean:
                 m, s = _return_meanstd(getattr(self, plot_keys[i]), axis=0)
-                plt.fill_between(self.f_axis, m-s, m+s, color=plot_color, alpha=0.5)
-                plt.loglog(self.f_axis, m, plot_color)
+                plt.fill_between(self.f_axis[p_inds], m[p_inds]-s[p_inds], m[p_inds]+s[p_inds], color=plot_color, alpha=0.5)
+                plt.loglog(self.f_axis[p_inds], m, plot_color[p_inds])
             else:
-                plt.loglog(self.f_axis, getattr(self, plot_keys[i])[plot_chan,:].T, plot_color, alpha=np.max([0.1, 0.75-(np.size(plot_chan)-1)/10]))
+                plt.loglog(self.f_axis[p_inds], getattr(self, plot_keys[i])[plot_chan,p_inds].T, plot_color, alpha=np.max([0.1, 0.75-(np.size(plot_chan)-1)/10]))
 
             plt.title(titles[i])
             plt.xlim(self.f_axis[1], self.f_axis[-1])
@@ -245,6 +252,12 @@ class LFPCA:
 
 
         plt.tight_layout()
+
+def _freq_to_ind(f_axis, exc_freqs):
+    exc_inds = []
+    for ef in exc_freqs:
+        exc_inds.append(np.where(np.logical_and(f_axis>=ef[0],f_axis<=ef[1]))[0])
+    return list(set(np.arange(len(f_axis)))-set(np.concatenate(np.array(exc_inds),axis=0)))
 
 def lfpca_load_spec(npz_filename):
     """ Load an .npz file to populate the computed spectral fields of lfpca
