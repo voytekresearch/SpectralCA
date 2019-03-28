ee17a5a3b45db914f5c369437c837abbbc59513f
ldliao <laura60629@gmail.com>
2019-03-28 23:19:13 +1300

    fixed hist

--- notebooks_visualization/lfpca_vis.py
+++ notebooks_visualization/lfpca_vis.py
@@ -16,7 +16,7 @@ from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import row, column
 from bokeh.models import ColumnDataSource, Slider, Span
 from bokeh.models.widgets import Select, Slider
-from ipywidgets import interact
+from ipywidgets import Layout, HBox, interactive
 
 output_notebook()
 
@@ -35,7 +35,7 @@ def plot_vis(lf, chan=0, select_freq=10, select_bin=20):
 
     # set up interact
     # f: frequency
-    def update_spct_hist(f=10, channel=0, numbins=20):
+    def update_spct_hist(channel=0, f=10, numbins=20):
         plot_chan = int(channel)
         plot_freq = np.where(lf.f_axis==f)[0][0]
         y, x = np.histogram(lf.spg[plot_chan,plot_freq,:], bins=numbins, density=True)
@@ -64,6 +64,9 @@ def plot_vis(lf, chan=0, select_freq=10, select_bin=20):
     # set up histogram
     y, x = np.histogram(lf.spg[0,10,:], bins=20, density=True)
     hist_fig = figure(plot_height=300, plot_width=300, x_axis_label='Power', y_axis_label='Probability')
+    hist_fig.axis.visible = False
+    hist_fig.title.text = 'Freq = %.1fHz, p-value = %.4f'%(10, lf.ks_pvals[int(1), 10])
+    hist_fig.axis.major_label_text_font_size= '0pt'
     hist_plot = hist_fig.quad(top=y, bottom=0, left=x[:-1], right=x[1:], fill_color='#295B99', line_color="#033649")
 
     fit_plot = hist_fig.line(x[:-1],y, line_width=8,alpha=0.7,line_color="#D53B54",legend='Fit PDF')
@@ -92,7 +95,10 @@ def plot_vis(lf, chan=0, select_freq=10, select_bin=20):
     psd_plot.add_layout(vline_psd)
     scv_plot.add_layout(vline_scv)
 
-    # set up layout and interact toola
+    # set up layout and interact tools 
     layout = column(row(psd_plot, scv_plot, hist_fig))
     show(layout, notebook_handle=True)
-    interact(update_spct_hist, f=(1,199), numbins=range(10,55,5), channel=(1,len(DEFAULT_TICKERS)-1))
+    widget = interactive(update_spct_hist, channel=range(1,len(DEFAULT_TICKERS)-1), f=(1,199), numbins=(10,55,5))
+    items = [kid for kid in widget.children]
+    display(HBox(children=items))
+
